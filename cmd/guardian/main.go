a20cb04f27d9bc74eee4cc3216a31539cd7f4621
Julian Friedman <julz.friedman@uk.ibm.com>
2015-09-08 15:09:49 +0100

    Add flags from garden-linux
    Signed-off-by: Gareth Smith <gsmith@pivotal.io>

--- cmd/guardian/main.go
+++ cmd/guardian/main.go
@@ -26,10 +26,34 @@ var listenAddr = flag.String(
 	"address to listen on",
 )
 
+var snapshotsPath = flag.String(
+	"snapshots",
+	"",
+	"directory in which to store container state to persist through restarts",
+)
+
+var binPath = flag.String(
+	"bin",
+	"",
+	"directory containing backend-specific scripts (i.e. ./create.sh)",
+)
+
 var depotPath = flag.String(
 	"depot",
 	"",
 	"directory in which to store containers",
+)
+
+var rootFSPath = flag.String(
+	"rootfs",
+	"",
+	"directory of the rootfs for the containers",
+)
+
+var disableQuotas = flag.Bool(
+	"disableQuotas",
+	false,
+	"disable disk quotas",
 )
 
 var graceTime = flag.Duration(
@@ -37,6 +61,98 @@ var graceTime = flag.Duration(
 	0,
 	"time after which to destroy idle containers",
 )
+
+var portPoolStart = flag.Uint(
+	"portPoolStart",
+	60000,
+	"start of ephemeral port range used for mapped container ports",
+)
+
+var portPoolSize = flag.Uint(
+	"portPoolSize",
+	5000,
+	"size of port pool used for mapped container ports",
+)
+
+var networkPool = flag.String("networkPool",
+	"10.254.0.0/22",
+	"Pool of dynamically allocated container subnets")
+
+var denyNetworks = flag.String(
+	"denyNetworks",
+	"",
+	"CIDR blocks representing IPs to blacklist",
+)
+
+var allowNetworks = flag.String(
+	"allowNetworks",
+	"",
+	"CIDR blocks representing IPs to whitelist",
+)
+
+var graphRoot = flag.String(
+	"graph",
+	"/var/lib/garden-docker-graph",
+	"docker image graph",
+)
+
+var dockerRegistry = flag.String(
+	"registry",
+	"",
+	///registry.IndexServerAddress(),
+	"docker registry API endpoint",
+)
+
+var insecureRegistries = flag.String(
+	"insecureDockerRegistryList",
+	"",
+	"comma-separated list of docker registries to allow connection to even if they are not secure",
+)
+
+var tag = flag.String(
+	"tag",
+	"",
+	"server-wide identifier used for 'global' configuration, must be less than 3 character long",
+)
+
+var dropsondeOrigin = flag.String(
+	"dropsondeOrigin",
+	"garden-linux",
+	"Origin identifier for dropsonde-emitted metrics.",
+)
+
+var dropsondeDestination = flag.String(
+	"dropsondeDestination",
+	"localhost:3457",
+	"Destination for dropsonde-emitted metrics.",
+)
+
+var allowHostAccess = flag.Bool(
+	"allowHostAccess",
+	false,
+	"allow network access to host",
+)
+
+var iptablesLogMethod = flag.String(
+	"iptablesLogMethod",
+	"kernel",
+	"type of iptable logging to use, one of 'kernel' or 'nflog' (default: kernel)",
+)
+
+var mtu = flag.Int(
+	"mtu",
+	1500,
+	"MTU size for container network interfaces")
+
+var externalIP = flag.String(
+	"externalIP",
+	"",
+	"IP address to use to reach container's mapped ports")
+
+var maxContainers = flag.Uint(
+	"maxContainers",
+	0,
+	"Maximum number of containers that can be created")
 
 func main() {
 	cf_debug_server.AddFlags(flag.CommandLine)
