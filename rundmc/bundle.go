38b320ea42d170a7e5e74f2238deb4339a4a24bf
Glyn Normington <gnormington@pivotal.io>
2015-09-22 15:09:55 +0100

    Add namespaces to runtime.json.
    [#102982762]
    
    Signed-off-by: Zhou Yu <zyu@pivotal.io>

--- rundmc/bundle.go
+++ rundmc/bundle.go
@@ -10,11 +10,27 @@ import (
 )
 
 type Bundle struct {
-	configJSON []byte
+	configJSON  []byte
+	runtimeJSON []byte
+}
+
+type namespaceType string
+
+type namespace struct {
+	NsType namespaceType `json:"type"`
+	Path   string        `json:"path"`
 }
 
 func (b Bundle) Create(path string) error {
-	return ioutil.WriteFile(filepath.Join(path, "config.json"), b.configJSON, 0700)
+	err := ioutil.WriteFile(filepath.Join(path, "config.json"), b.configJSON, 0700)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(filepath.Join(path, "runtime.json"), b.runtimeJSON, 0700)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func BundleForCmd(cmd *exec.Cmd) *Bundle {
@@ -30,7 +46,36 @@ func BundleForCmd(cmd *exec.Cmd) *Bundle {
 		panic(err) // can't happen
 	}
 
-	return &Bundle{
-		configJSON: configJson,
+	runtimeJson, err := json.Marshal(RuntimeSpec{
+		Linux: Linux{
+			Namespaces: namespaces(),
+		},
+	})
+	if err != nil {
+		panic(err) // can't happen
 	}
+
+	return &Bundle{
+		configJSON:  configJson,
+		runtimeJSON: runtimeJson,
+	}
+}
+
+func namespaces() []namespace {
+	namespaces := make([]namespace, 6)
+	nsTypes := []namespaceType{"pid", "network", "mount", "ipc", "uts", "user"}
+	for i, nsType := range nsTypes {
+		namespaces[i] = namespace{
+			NsType: nsType,
+			Path:   ""}
+	}
+	return namespaces
+}
+
+type RuntimeSpec struct {
+	Linux Linux `json:"linux"`
+}
+
+type Linux struct {
+	Namespaces []namespace `json:"namespaces"`
 }
--- rundmc/bundle_test.go
+++ rundmc/bundle_test.go
@@ -13,23 +13,91 @@ import (
 )
 
 var _ = Describe("BundleForCmd", func() {
-	It("creates a bundle with the specified command", func() {
-		tmp, err := ioutil.TempDir("", "bndle")
+
+	var (
+		bndle *rundmc.Bundle
+		tmp   string
+	)
+
+	BeforeEach(func() {
+		var err error
+		tmp, err = ioutil.TempDir("", "bndle")
 		Expect(err).NotTo(HaveOccurred())
-		defer os.RemoveAll(tmp)
 
-		bndle := rundmc.BundleForCmd(exec.Command("echo", "hello"))
-		bndle.Create(tmp)
+		bndle = rundmc.BundleForCmd(exec.Command("echo", "hello"))
+		Expect(bndle.Create(tmp)).To(Succeed())
+	})
 
-		configFile, err := os.Open(path.Join(tmp, "config.json"))
-		Expect(err).NotTo(HaveOccurred())
+	AfterEach(func() {
+		if tmp != "" {
+			os.RemoveAll(tmp)
+		}
+	})
 
-		config := make(map[string]interface{})
-		Expect(json.NewDecoder(configFile).Decode(&config)).To(Succeed())
+	Context("config.json", func() {
+		var (
+			config map[string]interface{}
+		)
 
-		Expect(config["process"]).To(HaveKeyWithValue(
-			BeEquivalentTo("args"),
-			ConsistOf("echo", "hello"),
-		))
+		BeforeEach(func() {
+			config = parseJson(path.Join(tmp, "config.json"))
+		})
+
+		It("should contain the specified command", func() {
+			Expect(config["process"]).To(HaveKeyWithValue(
+				BeEquivalentTo("args"),
+				ConsistOf("echo", "hello"),
+			))
+		})
+	})
+
+	Context("runtime.json", func() {
+		var (
+			runtime map[string]interface{}
+		)
+
+		BeforeEach(func() {
+			runtime = parseJson(path.Join(tmp, "runtime.json"))
+		})
+
+		Context("linux", func() {
+			var linux map[string]interface{}
+
+			BeforeEach(func() {
+				var ok bool
+				linux, ok = runtime["linux"].(map[string]interface{})
+				Expect(ok).To(BeTrue())
+			})
+
+			It("should configure all the namespaces", func() {
+				Expect(linux).To(HaveKey(
+					BeEquivalentTo("namespaces"),
+				))
+				namespaces, ok := linux["namespaces"].([]interface{})
+				Expect(ok).To(BeTrue())
+				Expect(namespaces).To(ConsistOf(expectedNamespaces()...))
+			})
+		})
+
 	})
 })
+
+func expectedNamespaces() []interface{} {
+	namespaces := make([]interface{}, 6)
+	nsTypes := []interface{}{"pid", "network", "mount", "ipc", "uts", "user"}
+	for i, NsType := range nsTypes {
+		nsMap := make(map[string]interface{}, 2)
+		nsMap["type"], nsMap["path"] = NsType, ""
+		namespaces[i] = nsMap
+	}
+	return namespaces
+}
+
+func parseJson(path string) map[string]interface{} {
+	Expect(path).To(BeAnExistingFile())
+	configFile, err := os.Open(path)
+	Expect(err).NotTo(HaveOccurred())
+	config := make(map[string]interface{})
+	Expect(json.NewDecoder(configFile).Decode(&config)).To(Succeed())
+	return config
+}
