1d84dcea4c0f6e6f72458ef5681fd1674327f42a
Petar Petrov <pppepito86@gmail.com>
2016-03-22 14:25:57 +0000

    Create `inspector-garden` tool to inspect graph contents
    Signed-off-by: Alberto Leal <aleal@pivotal.io>

--- gqt/gqt_suite_test.go
+++ gqt/gqt_suite_test.go
@@ -23,7 +23,7 @@ var defaultRuntime = map[string]string{
 
 var ginkgoIO = garden.ProcessIO{Stdout: GinkgoWriter, Stderr: GinkgoWriter}
 
-var ociRuntimeBin, gardenBin, initBin, kawasakiBin, iodaemonBin, nstarBin string
+var ociRuntimeBin, gardenBin, initBin, kawasakiBin, iodaemonBin, nstarBin, inspectorGardenBin string
 
 func TestGqt(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -50,6 +50,9 @@ func TestGqt(t *testing.T) {
 			bins["init_bin_path"], err = gexec.Build("github.com/cloudfoundry-incubator/guardian/cmd/init")
 			Expect(err).NotTo(HaveOccurred())
 
+			bins["inspector-garden_bin_path"], err = gexec.Build("github.com/cloudfoundry-incubator/guardian/cmd/inspector-garden")
+			Expect(err).NotTo(HaveOccurred())
+
 			cmd := exec.Command("make")
 			cmd.Dir = "../rundmc/nstar"
 			cmd.Stdout = GinkgoWriter
@@ -72,6 +75,7 @@ func TestGqt(t *testing.T) {
 		nstarBin = bins["nstar_bin_path"]
 		kawasakiBin = bins["kawasaki_bin_path"]
 		initBin = bins["init_bin_path"]
+		inspectorGardenBin = bins["inspector-garden_bin_path"]
 	})
 
 	BeforeEach(func() {
