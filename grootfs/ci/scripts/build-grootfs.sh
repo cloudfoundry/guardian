cdf12d66b56356f5ec9ff8d57cb234be89afdc9a
Alberto Leal <aleal@pivotal.io>
2016-07-25 16:18:44 +0100

    Create job to release a new version of grootfs
    [#126735873]
    
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

---
+++ grootfs/ci/scripts/build-grootfs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+BUILD_PATH=$PWD
+
+grootfsPath=/go/src/code.cloudfoundry.org/grootfs
+rmdir $grootfsPath
+ln -s $PWD/src/code.cloudfoundry.org/grootfs $grootfsPath
+cd $grootfsPath
+
+GOOS=linux go build -o grootfs .
+tar -cfz grootfs.tgz grootfs
+mv groofs.tgz $BUILD_PATH/build-grootfs/
