d30ab008db4d6c562aa6cb0fa09b1d71ba22d1ce
Gabe Rosenhouse <grosenhouse@pivotal.io>
2016-08-08 17:58:02 -0500

    Capture and log external networker's stderr
    [#127923587]

--- netplugin/external_networker.go
+++ netplugin/external_networker.go
@@ -66,6 +66,8 @@ func (p *ExternalBinaryNetworker) Network(log lager.Logger, containerSpec garden
 	cmd.Args = upArgs
 	cmdOutput := &bytes.Buffer{}
 	cmd.Stdout = cmdOutput
+	cmdStderr := &bytes.Buffer{}
+	cmd.Stderr = cmdStderr
 
 	input, err := cmd.StdinPipe()
 	if err != nil {
@@ -80,8 +82,11 @@ func (p *ExternalBinaryNetworker) Network(log lager.Logger, containerSpec garden
 
 	err = p.commandRunner.Run(cmd)
 	if err != nil {
+		log.Error("external-networker-result", err, lager.Data{"output": cmdStderr.String()})
 		return err
 	}
+
+	log.Info("external-networker-result", lager.Data{"output": cmdStderr.String()})
 
 	if len(cmdOutput.Bytes()) == 0 {
 		return nil
