6724143c848c65a62e2f66836f1033aee70c3c37
Georgi Sabev <georgethebeatle@gmail.com>
2016-08-24 15:58:10 +0300

    Wait for all output to be copied before exiting
    Close reference to slave in dadoo once container process is running
    
    [#128686087]
    
    Signed-off-by: Petar Petrov <pppepito86@gmail.com>

--- cmd/dadoo/main_linux.go
+++ cmd/dadoo/main_linux.go
@@ -11,6 +11,7 @@ import (
 	"os/signal"
 	"path/filepath"
 	"strconv"
+	"sync"
 	"syscall"
 	"time"
 
@@ -26,6 +27,8 @@ var gid = flag.Int("gid", 0, "gid to chown console to")
 var rows = flag.Int("rows", 0, "rows for tty")
 var cols = flag.Int("cols", 0, "cols for tty")
 var tty = flag.Bool("tty", false, "tty requested")
+
+var ioWg sync.WaitGroup = sync.WaitGroup{}
 
 func main() {
 	os.Exit(run())
@@ -100,6 +103,8 @@ func waitForContainerToExit(dir string, containerPid int, signals chan os.Signal
 					exitCode = 128 + int(status.Signal())
 				}
 
+				ioWg.Wait() // wait for full output to be collected
+
 				check(ioutil.WriteFile(filepath.Join(dir, "exitcode"), []byte(strconv.Itoa(exitCode)), 0700))
 				return exitCode
 			}
@@ -135,12 +140,13 @@ func setupTty(stdin io.Reader, stdout io.Writer, pidFilePath string, winszFifo i
 		check(err)
 	}
 
-	go io.Copy(stdout, m)
-
+	ioWg.Add(1)
 	go func() {
-		io.Copy(m, stdin)
-		m.Close()
+		defer ioWg.Done()
+		io.Copy(stdout, m)
 	}()
+
+	go io.Copy(m, stdin)
 
 	dadoo.SetWinSize(m, defaultWinSize)
 
@@ -151,6 +157,9 @@ func setupTty(stdin io.Reader, stdout io.Writer, pidFilePath string, winszFifo i
 				println("Timed out trying to open pidfile: ", err.Error())
 				return
 			}
+
+			// free up slave fd as soon as container process is running to avoid hanging
+			s.Close()
 
 			p, err := os.FindProcess(pid)
 			check(err) // cant happen on linux
