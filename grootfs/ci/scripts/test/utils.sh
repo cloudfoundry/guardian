a6004f2bf9b7ce821f5fc4675e6e513be0ed00d3
Claudia Beresford <cberesford@pivotal.io>
2016-08-25 15:21:08 +0100

    Add grootfs-bench tests to CI
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- grootfs/ci/scripts/test/utils.sh
+++ grootfs/ci/scripts/test/utils.sh
@@ -28,13 +28,6 @@ function sudo_mount_btrfs {
   sudo bash -c "$MOUNT_BTRFS_FUNC; mount_btrfs"
 }
 
-function show_groot_banner {
-  cat $(dirname $BASH_SOURCE)/groot.ascii
-  echo
-  echo "I AM $(whoami)"
-  echo
-}
-
 function move_to_gopath {
   grootfsPath=/go/src/code.cloudfoundry.org/grootfs
   rmdir $grootfsPath
