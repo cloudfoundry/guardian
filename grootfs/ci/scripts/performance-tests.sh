07741e2090cff251659d5710d46f7a788e3b0e85
Alberto Leal <aleal@pivotal.io>
2016-09-28 14:28:32 +0100

    Run performance tests daily
    [#127950553]
    
    Signed-off-by: Claudia Beresford <cberesford@pivotal.io>

--- grootfs/ci/scripts/performance-tests.sh
+++ grootfs/ci/scripts/performance-tests.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 set -e
 
+env
+
 source $(dirname $BASH_SOURCE)/test/utils.sh
 sudo_mount_btrfs
 
 grootfs_path=$(move_to_gopath grootfs)
 grootfs_bench_path=$(move_to_gopath grootfs-bench)
+grootfs_bench_performance_path="$grootfs_bench_path/performance"
 
 pushd $grootfs_path
   install_dependencies
   make
 popd
 
-cd $grootfs_bench_path
-install_dependencies
-make
+pushd $grootfs_bench_path
+  install_dependencies
+  make
+popd
 
-perf_test_image="docker:///busybox"
-# warm the cache
-../grootfs/grootfs --store /mnt/btrfs create $perf_test_image my-warm-box > /dev/null
+pushd /go/src/code.cloudfoundry.org/grootfs-bench/performance
+  go build -o grootfs-performance-runner .
 
-echo "RUNNING PERFORMANCE TESTS, STAND BY..." | grootsay
+  perf_test_image="docker:///busybox"
+  # warm the cache
+  ../../grootfs/grootfs --store /mnt/btrfs create $perf_test_image my-warm-box &> /dev/null
 
-./grootfs-bench --gbin ../grootfs/grootfs --store /mnt/btrfs --concurrency 5 --image $perf_test_image --nospin $@
+  echo "RUNNING PERFORMANCE TESTS, STAND BY..." | grootsay
+
+  ./grootfs-performance-runner \
+    --benchBinPath ../grootfs-bench \
+    [--gbin ../../grootfs/grootfs \
+    --store /mnt/btrfs \
+    --concurrency 5 \
+    --image $perf_test_image \
+    --nospin \
+    --json $@]
+popd
