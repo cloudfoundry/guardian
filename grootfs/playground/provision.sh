a8dddb1d066ff81582075fffd62607fd7ab60012
George Lestaris <glestaris@pivotal.io>
2016-10-07 16:45:41 +0100

    Remove vim/bash-it from playground
    * `ubuntu` user has a configured GOPATH

--- grootfs/playground/provision.sh
+++ grootfs/playground/provision.sh
@@ -1,71 +1,86 @@
 #!/bin/bash
+set -e
+set -x
 
-set -e -x
+install_apt_packages() {
+  apt-get -y update
+  apt-get -y clean
+  apt-get install -y \
+    gcc make \
+    vim-nox \
+    git silversearcher-ag curl jq \
+    btrfs-tools uidmap
+}
 
-apt-get -y update
-apt-get -y clean
+install_go() {
+  source /root/.go_env || true
+  if which go; then
+    return
+  fi
 
-apt-get install -y curl git gcc make python-dev vim-nox jq btrfs-tools uidmap silversearcher-ag
+  wget -qO- https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz | tar -C /usr/local -xzf -
+}
 
-wget -qO- https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz | tar -C /usr/local -xzf -
+configure_user() {
+  user=${1:-$(whoami)}
+  home=$(eval "echo ~$user")
 
-# Set up vim for golang development
-if [ ! -d $HOME/.vim ]; then
-  git clone https://github.com/luan/vimfiles.git $HOME/.vim
-  $HOME/.vim/install --non-interactive
-fi
+  source $home/.go_env || true
+  if which go; then
+    return
+  fi
 
-# Set up bash-it
-if [ ! -d $HOME/.bash_it ]; then
-  git clone --depth=1 https://github.com/Bash-it/bash-it.git $HOME/.bash_it
-  $HOME/.bash_it/install.sh -s
-fi
-
-# Set up git aliases
-cat > $HOME/.bash_it/custom/git_config.bash <<EOF
-#!/usr/bin/env bash
-git config --global core.editor vim
-git config --global core.pager "less -FXRS -x2"
-git config --global alias.co checkout
-git config --global alias.st status
-git config --global alias.b branch
-git config --global alias.plog "log --graph --abbrev-commit --decorate --date=relative --format=format:\'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
-git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
-git config --global alias.flog = log --pretty=fuller --decorate
+  # Set up $GOPATH and add go executables to $PATH
+  sudo -u $user cat > $home/.go_env <<EOF
+export GOPATH=$home/go
+export GOROOT=/usr/local/go
+export PATH=\$PATH:\$GOPATH/bin:\$GOROOT/bin
 EOF
-
-#Set up $GOPATH and add go executables to $PATH
-cat > $HOME/.bash_it/custom/go_env.bash <<EOF
-#!/usr/bin/env bash
-export GOPATH=/home/ubuntu/go
-export PATH=/home/ubuntu/go/bin:/usr/local/go/bin:$PATH
+  sudo -u $user cat >> $home/.bashrc <<EOF
+source \$HOME/.go_env
 EOF
-source $HOME/.bash_it/custom/go_env.bash
+  sudo -u $user mkdir -p $home/go
+  sudo -u $user mkdir -p $home/go/bin
+}
 
-# Install awesome grootsay, I gotta say
-go get github.com/fouralarmfire/grootsay
+install_groot() {
+  if which groot; then
+    return
+  fi
 
-chown ubuntu:ubuntu $GOPATH
+  source /root/.go_env
+  # Install Management Dependendy tool
+  curl -s https://glide.sh/get | sh
 
-# Clone them all
-go get code.cloudfoundry.org/grootfs
-go get code.cloudfoundry.org/grootfs-bench
-GROOT_PATH=$GOPATH/src/code.cloudfoundry.org/grootfs
+  # Install awesome grootsay, I gotta say
+  go get github.com/fouralarmfire/grootsay
 
-# Install Management Dependendy tool
-curl https://glide.sh/get | sh
+  # Clone them all
+  go get code.cloudfoundry.org/grootfs
+  go get code.cloudfoundry.org/grootfs-bench
 
-# Build grootfs and move binaries around
-pushd $GROOT_PATH
-  make deps
-  make
-  cp {grootfs,drax} /usr/local/bin/
-  cp hack/{quick-setup,cleanup-store} /usr/local/bin/
-popd
+  # Build grootfs and move binaries around
+  pushd $HOME/go/src/code.cloudfoundry.org/grootfs
+    make deps
+    make
+    cp {grootfs,drax} /usr/local/bin
+    cp hack/{quick-setup,cleanup-store} /usr/local/bin
+  popd
+}
 
-# Configure btrfs environment
-if [ ! -f /root/btrfs_volume ]; then
+setup_btrfs() {
+  if [ -d /var/lib/grootfs ]; then
+    return
+  fi
+
+  # Configure btrfs environment
   quick-setup
-fi
+}
 
+install_apt_packages
+install_go
+configure_user "root"
+configure_user "ubuntu"
+install_groot
+setup_btrfs
 echo "Groot to go. Enjoy your day."
