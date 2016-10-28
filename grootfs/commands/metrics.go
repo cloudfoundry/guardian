37976cd14bc21122fb54191368d8e988cf95e373
Tiago Scolari <tscolari@pivotal.io>
2016-10-28 14:51:39 +0100

    Allow metrics to also run against a bundle path
    [finishes #133089701]
    
    Signed-off-by: Alberto Leal <aleal@pivotal.io>

--- grootfs/commands/metrics.go
+++ grootfs/commands/metrics.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 
+	"code.cloudfoundry.org/grootfs/commands/idfinder"
 	"code.cloudfoundry.org/grootfs/commands/storepath"
 	"code.cloudfoundry.org/grootfs/groot"
 	bundlerpkg "code.cloudfoundry.org/grootfs/store/bundler"
@@ -24,16 +25,26 @@ var MetricsCommand = cli.Command{
 		logger := ctx.App.Metadata["logger"].(lager.Logger)
 		logger = logger.Session("metrics")
 
+		storePath := ctx.GlobalString("store")
 		if ctx.NArg() != 1 {
 			logger.Error("parsing-command", errors.New("invalid arguments"), lager.Data{"args": ctx.Args()})
 			return cli.NewExitError(fmt.Sprintf("invalid arguments - usage: %s", ctx.Command.Usage), 1)
 		}
 
-		storePath, err := storepath.UserBased(ctx.GlobalString("store"))
+		idOrPath := ctx.Args().First()
+		id, err := idfinder.FindID(storePath, idOrPath)
+		if err != nil {
+			return cli.NewExitError(err.Error(), 1)
+		}
+
+		if id == idOrPath {
+			storePath, err = storepath.UserBased(storePath)
+		} else {
+			storePath, err = idfinder.FindSubStorePath(storePath, idOrPath)
+		}
 		if err != nil {
 			return cli.NewExitError(fmt.Sprintf("can't determine the store path: %s", err.Error()), 1)
 		}
-		id := ctx.Args().First()
 
 		btrfsVolumeDriver := volume_driver.NewBtrfs(ctx.GlobalString("drax-bin"), storePath)
 		bundler := bundlerpkg.NewBundler(btrfsVolumeDriver, storePath)
--- grootfs/integration/groot/metrics_test.go
+++ grootfs/integration/groot/metrics_test.go
@@ -6,7 +6,9 @@ import (
 	"os/exec"
 	"path/filepath"
 
+	"code.cloudfoundry.org/grootfs/groot"
 	"code.cloudfoundry.org/grootfs/integration"
+	"code.cloudfoundry.org/grootfs/store"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -15,7 +17,10 @@ import (
 )
 
 var _ = Describe("Metrics", func() {
-	var imagePath string
+	var (
+		imagePath string
+		bundle    groot.Bundle
+	)
 
 	BeforeEach(func() {
 		var err error
@@ -23,34 +28,73 @@ var _ = Describe("Metrics", func() {
 		Expect(err).NotTo(HaveOccurred())
 	})
 
-	It("returns the metrics for given bundle", func() {
-		cmd := exec.Command("dd", "if=/dev/zero", fmt.Sprintf("of=%s", filepath.Join(imagePath, "fatfile")), "bs=1048576", "count=5")
-		sess, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
-		Expect(err).ToNot(HaveOccurred())
-		Eventually(sess).Should(gexec.Exit(0))
+	Context("when bundle exists", func() {
+		BeforeEach(func() {
+			cmd := exec.Command("dd", "if=/dev/zero", fmt.Sprintf("of=%s", filepath.Join(imagePath, "fatfile")), "bs=1048576", "count=5")
+			sess, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).ToNot(HaveOccurred())
+			Eventually(sess).Should(gexec.Exit(0))
 
-		bundle := integration.CreateBundle(GrootFSBin, StorePath, DraxBin, imagePath, "random-id", 0)
+			bundle = integration.CreateBundle(GrootFSBin, StorePath, DraxBin, imagePath, "random-id", 0)
+			cmd = exec.Command("dd", "if=/dev/zero", fmt.Sprintf("of=%s", filepath.Join(bundle.RootFSPath, "hello")), "bs=1048576", "count=4")
+			sess, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).ToNot(HaveOccurred())
+			Eventually(sess).Should(gexec.Exit(0))
+		})
 
-		cmd = exec.Command("dd", "if=/dev/zero", fmt.Sprintf("of=%s", filepath.Join(bundle.RootFSPath, "hello")), "bs=1048576", "count=4")
-		sess, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
-		Expect(err).ToNot(HaveOccurred())
-		Eventually(sess).Should(gexec.Exit(0))
+		Context("when the last parameter is the bundle ID", func() {
+			It("returns the metrics for given bundle id", func() {
+				cmd := exec.Command(GrootFSBin, "--store", StorePath, "--drax-bin", DraxBin, "metrics", "random-id")
+				sess, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+				Expect(err).ToNot(HaveOccurred())
+				Eventually(sess).Should(gexec.Exit(0))
 
-		cmd = exec.Command(GrootFSBin, "--store", StorePath, "--drax-bin", DraxBin, "metrics", "random-id")
-		sess, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
-		Expect(err).ToNot(HaveOccurred())
-		Eventually(sess).Should(gexec.Exit(0))
+				Eventually(sess.Out).Should(gbytes.Say(`{"disk_usage":{"total_bytes_used":9453568,"exclusive_bytes_used":4210688}}`))
+			})
+		})
 
-		Eventually(sess.Out).Should(gbytes.Say(`{"disk_usage":{"total_bytes_used":9453568,"exclusive_bytes_used":4210688}}`))
+		Context("when the last parameter is the bundle path", func() {
+			It("returns the metrics for given bundle path", func() {
+				cmd := exec.Command(GrootFSBin, "--log-level", "debug", "--store", StorePath, "--drax-bin", DraxBin, "metrics", bundle.Path)
+				sess, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+				Expect(err).ToNot(HaveOccurred())
+				Eventually(sess).Should(gexec.Exit(0))
+
+				Eventually(sess.Out).Should(gbytes.Say(`{"disk_usage":{"total_bytes_used":9453568,"exclusive_bytes_used":4210688}}`))
+			})
+		})
 	})
 
 	Context("when the bundle id doesn't exist", func() {
-		It("returns an error", func() {
-			cmd := exec.Command(GrootFSBin, "--store", StorePath, "--drax-bin", DraxBin, "metrics", "invalid-id")
-			sess, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
-			Expect(err).ToNot(HaveOccurred())
-			Eventually(sess).Should(gexec.Exit(1))
-			Eventually(sess.Out).Should(gbytes.Say("No such file or directory"))
+		Context("when the last parameter is a bundle id", func() {
+			It("returns an error", func() {
+				cmd := exec.Command(GrootFSBin, "--store", StorePath, "--drax-bin", DraxBin, "metrics", "invalid-id")
+				sess, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+				Expect(err).ToNot(HaveOccurred())
+				Eventually(sess).Should(gexec.Exit(1))
+				Eventually(sess.Out).Should(gbytes.Say("No such file or directory"))
+			})
+		})
+
+		Context("when the last parameter is a path", func() {
+			It("returns an error", func() {
+				bundlePath := filepath.Join(StorePath, CurrentUserID, store.BUNDLES_DIR_NAME, "not-here")
+				cmd := exec.Command(GrootFSBin, "--store", StorePath, "--drax-bin", DraxBin, "metrics", bundlePath)
+				sess, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+				Expect(err).ToNot(HaveOccurred())
+				Eventually(sess).Should(gexec.Exit(1))
+				Eventually(sess.Out).Should(gbytes.Say("No such file or directory"))
+			})
+
+			Context("when the path provided doesn't belong to the `--store` provided", func() {
+				It("returns an error", func() {
+					cmd := exec.Command(GrootFSBin, "--store", StorePath, "metrics", "/Iamnot/in/the/storage/bundles/1234/rootfs")
+					sess, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+					Expect(err).ToNot(HaveOccurred())
+					Eventually(sess).Should(gexec.Exit(1))
+					Eventually(sess.Out).Should(gbytes.Say("path `/Iamnot/in/the/storage/bundles/1234/rootfs` is outside store path"))
+				})
+			})
 		})
 	})
 
