66977e2475cbd22e391cafe5e84c1f05e150de58
Claudia Beresford <cberesford@pivotal.io>
2016-11-09 10:36:05 +0000

    Update grootfs build script to build from bosh release
    * Add drax binary to the build
    
    [#132631769]
    
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- grootfs/ci/scripts/build-grootfs.sh
+++ grootfs/ci/scripts/build-grootfs.sh
@@ -4,9 +4,9 @@ set -e -x
 
 OUTPUT_PATH=$PWD/build-grootfs
 GOPATH=$GOPATH:$PWD
-VERSION=$(cat grootfs-version/number)
+VERSION=$(cat grootfs-release-version/number)
 
-cd src/code.cloudfoundry.org/grootfs
+cd grootfs-release-master/src/code.cloudfoundry.org/grootfs
 
 # Update CLI version.
 grep grootfs.Version main.go | cut -d "=" -f2 | xargs -I % sed -i s:%:"${VERSION}":g main.go
@@ -18,7 +18,8 @@ git config --global user.name "GrootFS CI Bot"
 git add main.go
 git commit -m "Bump version to ${VERSION}"
 
-GOOS=linux go build -o grootfs .
+make
 tar -czf grootfs.tgz grootfs
+tar -czf drax.tgz drax
 
 cp -r . $OUTPUT_PATH
