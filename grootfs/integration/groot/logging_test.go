e1d603b47d394311d939312bdec0a59e9ee69cfa
George Lestaris <glestaris@pivotal.io>
2016-11-10 17:34:26 +0000

    Move flag tests to logging tests
    [#133832779]

--- grootfs/integration/groot/logging_test.go
+++ grootfs/integration/groot/logging_test.go
@@ -46,4 +46,64 @@ var _ = Describe("Logging", func() {
 
 		Expect(logBuffer).To(gbytes.Say("namespaced-unpacking.unpack"))
 	})
+
+	Describe("--log-level and --log-file flags", func() {
+		Context("when the --log-file is not set", func() {
+			Context("and --log-level is set", func() {
+				It("writes logs to stderr", func() {
+					buffer := gbytes.NewBuffer()
+
+					_, err := Runner.
+						WithStderr(buffer).
+						WithLogLevel(lager.DEBUG).
+						Create(groot.CreateSpec{
+							ID:        "my-image",
+							BaseImage: "/non/existent/rootfs",
+						})
+					Expect(err).To(HaveOccurred())
+
+					Expect(buffer).To(gbytes.Say(`"error":".*no such file or directory"`))
+				})
+			})
+		})
+
+		Context("when the --log-file is set", func() {
+			var (
+				logFilePath string
+				logReader   io.ReadCloser
+				logWriter   io.WriteCloser
+			)
+
+			BeforeEach(func() {
+				r, w, err := os.Pipe()
+				Expect(err).NotTo(HaveOccurred())
+
+				logFilePath = fmt.Sprintf("/proc/%d/fd/%d", os.Getpid(), w.Fd())
+				logReader = r
+				logWriter = w
+			})
+
+			AfterEach(func() {
+				Expect(logReader.Close()).To(Succeed())
+			})
+
+			Context("and --log-level is set", func() {
+				It("forwards logs to the given file", func() {
+					_, err := Runner.
+						WithLogFile(logFilePath).
+						WithLogLevel(lager.DEBUG).
+						Create(groot.CreateSpec{
+							ID:        "my-image",
+							BaseImage: "/non/existent/rootfs",
+						})
+					Expect(err).To(HaveOccurred())
+
+					Expect(logWriter.Close()).To(Succeed())
+					allTheLogs, err := ioutil.ReadAll(logReader)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(string(allTheLogs)).To(ContainSubstring("\"log_level\":0"))
+				})
+			})
+		})
+	})
 })
