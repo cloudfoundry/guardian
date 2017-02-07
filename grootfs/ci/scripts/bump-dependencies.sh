9b04c4c7c3e90db8ba85d3afd21f08d37dc1cf5a
CF Garden <garden@pivotal.io>
2017-02-07 15:58:07 +0000

    Added bump-dependencies

---
+++ grootfs/ci/scripts/bump-dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git clone grootfs grootfs-updated
+cd grootfs-updated
+./script/deps -u  github.com/containers/image
+
+# Commit if changes
+
+if [[ `git diff --cached` ]]; then
+  git config --global user.email "cf-garden+garden-gnome@pivotal.io"
+  git config --global user.name "I am Groot CI"
+  grootfs_changes=$(git diff --cached | tail -n +2)
+  git commit -m "$(printf "Bump dependencies\n\n${grootfs_changes}")"
+else
+  echo "No changes"
+fi
