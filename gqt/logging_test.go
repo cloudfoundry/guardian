dc5c4e013e6e155ea444185e774ef29585307a56
Will Martin <wmartin@pivotal.io>
2017-03-21 11:44:39 +0000

    Bump max log messages allowed in gqt
    Signed-off-by: Craig Furman <cfurman@pivotal.io>

--- gqt/logging_test.go
+++ gqt/logging_test.go
@@ -147,7 +147,7 @@ var _ = Describe("garden server Logging", func() {
 
 			It("doesn't log too many messages", func() {
 				outLines := strings.Split(string(client.Buffer().Contents()), "\n")
-				Expect(len(outLines)).To(BeNumerically("<", 300))
+				Expect(len(outLines)).To(BeNumerically("<", 400))
 			})
 		})
 	})
