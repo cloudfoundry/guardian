a83554cfe48e89accc1f3b7717abc46293c56646
Ed King <eking@pivotal.io>
2017-05-02 11:01:39 +0100

    Add MappingsForUser func
    [#144028139]
    
    Signed-off-by: Craig Furman <cfurman@pivotal.io>

--- idmapper/idmapping_test.go
+++ idmapper/idmapping_test.go
@@ -3,10 +3,37 @@ package idmapper_test
 import (
 	. "code.cloudfoundry.org/idmapper"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Idmapping", func() {
+	const maxID = uint32(4294967294)
+	DescribeTable(
+		"MappingsForUser",
+		func(id uint32, expectedMappings MappingList) {
+			Expect(MappingsForUser(id, maxID)).To(Equal(expectedMappings))
+			println(MappingsForUser(id, maxID).String())
+		},
+		Entry("id = 0", uint32(0), MappingList{
+			{ContainerID: 0, HostID: maxID, Size: 1},
+			{ContainerID: 1, HostID: 1, Size: maxID - 1},
+		}),
+		Entry("id = 1", uint32(1), MappingList{
+			{ContainerID: 0, HostID: 1, Size: 1},
+			{ContainerID: 1, HostID: 2, Size: maxID - 1},
+		}),
+		Entry("id = 1000", uint32(1000), MappingList{
+			{ContainerID: 0, HostID: 1000, Size: 1},
+			{ContainerID: 1, HostID: 1, Size: 999},
+			{ContainerID: 1000, HostID: 1001, Size: maxID - 1000},
+		}),
+		Entry("id = maxID", maxID, MappingList{
+			{ContainerID: 0, HostID: maxID, Size: 1},
+			{ContainerID: 1, HostID: 1, Size: maxID - 1},
+		}),
+	)
+
 	Describe("Map", func() {
 		Context("when the mapping does not contain the given id", func() {
 			It("returns the original id", func() {
