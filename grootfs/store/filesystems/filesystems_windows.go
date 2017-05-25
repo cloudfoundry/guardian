8a8052c0e5eb06f2bab9718f86e86f77eed9bfa2
Tiago Scolari <tscolari@pivotal.io>
2017-05-25 11:44:20 +0100

    Fix windows compilation error

--- grootfs/store/filesystems/filesystems_windows.go
+++ grootfs/store/filesystems/filesystems_windows.go
@@ -2,6 +2,6 @@ package filesystems
 
 import "errors"
 
-func CheckFSPath(path string, expectedFilesystem int64) error {
+func CheckFSPath(path string, expectedFilesystem int64, mountOptions ...string) error {
 	return errors.New("Implement me")
 }
