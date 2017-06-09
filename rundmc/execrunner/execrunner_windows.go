7c443cd4913b50d374b140f45f4365876c57cc13
Sam Smith <sesmith177@gmail.com>
2017-06-09 11:09:30 -0400

    Implement Wait() in windows execrunner
    - use RW mutex to synchronize access to process info
    
    [#146616901]
    
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- rundmc/execrunner/execrunner_windows.go
+++ rundmc/execrunner/execrunner_windows.go
@@ -2,9 +2,12 @@ package execrunner
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"sync"
+	"syscall"
 
 	"github.com/pkg/errors"
 
@@ -18,12 +21,6 @@ type DirectExecRunner struct {
 	RuntimePath   string
 	CommandRunner commandrunner.CommandRunner
 	ProcessIDGen  runrunc.UidGenerator
-	FileWriter    FileWriter
-}
-
-//go:generate counterfeiter . FileWriter
-type FileWriter interface {
-	WriteFile(path string, contents []byte, mode os.FileMode) error
 }
 
 func (e *DirectExecRunner) Run(log lager.Logger, processID string, spec *runrunc.PreparedSpec, bundlePath, processesPath, handle string, tty *garden.TTYSpec, io garden.ProcessIO) (garden.Process, error) {
@@ -37,15 +34,40 @@ func (e *DirectExecRunner) Run(log lager.Logger, processID string, spec *runrunc
 		return nil, errors.Wrap(err, "marshaling process spec")
 	}
 
-	specPath := filepath.Join(processesPath, proc.ID(), "spec.json")
-	if err := e.FileWriter.WriteFile(specPath, marshaledSpec, 0600); err != nil {
+	processPath := filepath.Join(processesPath, proc.ID())
+	specPath := filepath.Join(processPath, "spec.json")
+
+	if err := os.MkdirAll(processPath, 0700); err != nil {
+		return nil, err
+	}
+
+	if err := ioutil.WriteFile(specPath, marshaledSpec, 0600); err != nil {
 		return nil, errors.Wrap(err, "writing process spec")
 	}
 
-	cmd := exec.Command(e.RuntimePath, "--debug", "--log", "IGNORED", "exec", "-d", "-p", specPath, "--pid-file", "IGNORED", handle)
+	cmd := exec.Command(e.RuntimePath, "--debug", "--log", "IGNORED", "exec", "-p", specPath, "--pid-file", "IGNORED", handle)
 	if err := e.CommandRunner.Start(cmd); err != nil {
 		return nil, errors.Wrap(err, "execing runtime plugin")
 	}
+
+	proc.mux.Lock()
+
+	go func() {
+		if err := e.CommandRunner.Wait(cmd); err != nil {
+			if exitErr, ok := err.(*exec.ExitError); ok {
+				if status, ok := exitErr.Sys().(syscall.WaitStatus); ok {
+					proc.exitCode = status.ExitStatus()
+				} else {
+					proc.exitCode = 1
+					proc.exitErr = errors.New("couldn't get WaitStatus")
+				}
+			} else {
+				proc.exitCode = 1
+				proc.exitErr = err
+			}
+		}
+		proc.mux.Unlock()
+	}()
 
 	return proc, nil
 }
@@ -55,7 +77,10 @@ func (e *DirectExecRunner) Attach(log lager.Logger, processID string, io garden.
 }
 
 type process struct {
-	id string
+	id       string
+	exitCode int
+	exitErr  error
+	mux      sync.RWMutex
 }
 
 func (p *process) ID() string {
@@ -63,7 +88,10 @@ func (p *process) ID() string {
 }
 
 func (p *process) Wait() (int, error) {
-	return 0, nil
+	p.mux.RLock()
+	defer p.mux.RUnlock()
+
+	return p.exitCode, p.exitErr
 }
 
 func (p *process) SetTTY(ttySpec garden.TTYSpec) error {
