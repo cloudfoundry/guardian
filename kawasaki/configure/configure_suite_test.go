7d440ef91591bfc82caf90974079c0ce2b598c98
Craig Furman <cfurman@pivotal.io>
2017-06-14 15:57:06 +0200

    Increase the timeout kawasaki/configure tests
    [#146163395]
    
    Signed-off-by: Konstantinos Karampogias <konstantinos.karampogias@swisscom.com>

--- kawasaki/configure/configure_suite_test.go
+++ kawasaki/configure/configure_suite_test.go
@@ -1,6 +1,8 @@
 package configure_test
 
 import (
+	"time"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -8,6 +10,7 @@ import (
 )
 
 func TestConfigure(t *testing.T) {
+	SetDefaultEventuallyTimeout(2 * time.Second)
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Configure Suite")
 }
