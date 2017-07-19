5ba65b9ff4bd2c85248b3da664550c5523a75fca
Sam Smith <sesmith177@gmail.com>
2017-07-19 16:34:04 -0400

    Add Windows bundle
    cf push should respect memory limits when running on winc.
    `spec.Windows` is used by winc to apply mem limits.
    
    [#147778393]
    
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

---
+++ rundmc/bundlerules/windows.go
@@ -0,0 +1,16 @@
+package bundlerules
+
+import (
+	"code.cloudfoundry.org/guardian/gardener"
+	"code.cloudfoundry.org/guardian/rundmc/goci"
+	"github.com/opencontainers/runtime-spec/specs-go"
+)
+
+type Windows struct{}
+
+func (w Windows) Apply(bndl goci.Bndl, spec gardener.DesiredContainerSpec, _ string) (goci.Bndl, error) {
+	limit := uint64(spec.Limits.Memory.LimitInBytes)
+	bndl = bndl.WithWindowsMemoryLimit(specs.WindowsMemoryResources{Limit: &limit})
+
+	return bndl, nil
+}
