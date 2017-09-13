0d6ac39a49f253c5c754a40e7f8d15eeb1674838
Georgi Sabev <georgethebeatle@gmail.com>
2017-09-13 12:13:07 +0300

    Add big image test suite
    [#150473920]
    
    Signed-off-by: Claudia Beresford <cberesford@pivotal.io>

---
+++ grootfs/ci/scripts/big-image-test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+source $(dirname $BASH_SOURCE)/test/utils.sh
+
+trap unmount_storage EXIT
+
+mount_storage
+
+dest_path=$(move_to_gopath grootfs)
+cd $dest_path
+
+chmod +s /usr/bin/newuidmap
+chmod +s /usr/bin/newgidmap
+
+echo "I AM INTEGRATION: ${VOLUME_DRIVER} (${GROOTFS_TEST_UID}:${GROOTFS_TEST_GID})" | grootsay
+
+args=$@
+[ "$args" == "" ] && args="-r integration/big-image-experiment/"
+ginkgo -p -race $args
