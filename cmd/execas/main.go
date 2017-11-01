08df9523ba8e3af2632ac1f6bf9773becb0526fe
Julia Nedialkova <julianedialkova@hotmail.com>
2017-11-01 14:55:02 +0000

    execas execs a process with specified UID/GID
    We would rather stop using `chpst` from runit, as it adds the primary
    GID to the list of supplementary GIDs.
    
    [#152439956]
    
    Signed-off-by: Craig Furman <cfurman@pivotal.io>

---
+++ cmd/execas/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"syscall"
+)
+
+func main() {
+	uid := flag.Int64("uid", -1, "uid to run process as")
+	gid := flag.Int64("gid", -1, "gid to run process as")
+	flag.Parse()
+	if *uid == -1 {
+		fmt.Println("please pass the --uid flag")
+		os.Exit(1)
+	}
+	if *gid == -1 {
+		fmt.Println("please pass the --gid flag")
+		os.Exit(1)
+	}
+
+	_, _, err := syscall.Syscall(syscall.SYS_SETGID, uintptr(*gid), 0, 0)
+	if err != 0 {
+		must("setgid", err)
+	}
+	_, _, err = syscall.Syscall(syscall.SYS_SETUID, uintptr(*uid), 0, 0)
+	if err != 0 {
+		must("setuid", err)
+	}
+
+	cmdArgv := flag.Args()
+	must("exec", syscall.Exec(cmdArgv[0], cmdArgv, os.Environ()))
+	panic("unreachable")
+}
+
+func must(action string, err error) {
+	if err != nil {
+		fmt.Printf("error %s: %s\n", action, err)
+		os.Exit(1)
+	}
+}
