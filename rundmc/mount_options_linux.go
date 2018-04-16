12d476033d44cf33b208b1f01b188d86bb7a4478
CF Garden <garden@pivotal.io>
2018-04-16 18:39:00 +0300

    Revert "Bind mount is supported for arbitrary files/dirs"
    This reverts commit 39be3b8a1f9d818a59d394fc9b281b09b90cf62f.

--- rundmc/mount_options_linux.go
+++ rundmc/mount_options_linux.go
@@ -2,6 +2,7 @@ package rundmc
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/docker/docker/pkg/mount"
@@ -11,19 +12,11 @@ func (g MountOptionsGetter) GetMountOptions(path string) ([]string, error) {
 	return g(path)
 }
 
-func (c MountPointChecker) IsMountPoint(path string) (bool, error) {
-	return c(path)
-}
-
-func IsMountPoint(path string) (bool, error) {
-	mountInfo, err := getMountInfo(path)
-	if err != nil {
-		return false, err
-	}
-	return mountInfo != nil, nil
-}
-
 func GetMountOptions(path string) ([]string, error) {
+	if err := verifyExistingDirectory(path); err != nil {
+		return nil, err
+	}
+
 	mountInfo, err := getMountInfo(path)
 	if err != nil {
 		return nil, err
@@ -49,4 +42,17 @@ func getMountInfo(path string) (*mount.Info, error) {
 	}
 
 	return nil, nil
+}
+
+func verifyExistingDirectory(path string) error {
+	stat, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+
+	if !stat.IsDir() {
+		return fmt.Errorf("%s is not a directory", path)
+	}
+
+	return nil
 }
