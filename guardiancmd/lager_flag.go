28e336a055f2848872a0f9293528a0a945e5d7ae
Nima Kaviani <nkavian@us.ibm.com>
2018-05-15 15:09:25 -0700

    allow for pretty formatting of timestamps in logger
    [#156774350]
    
    Signed-off-by: Stephane Jolicoeur <sjolicoeur@pivotal.io>

--- guardiancmd/lager_flag.go
+++ guardiancmd/lager_flag.go
@@ -12,10 +12,14 @@ const (
 	LogLevelInfo  = "info"
 	LogLevelError = "error"
 	LogLevelFatal = "fatal"
+
+	FormatUnixEpoch = "unix-epoch"
+	FormatRFC3339   = "rfc3339"
 )
 
 type LagerFlag struct {
-	LogLevel string `long:"log-level" default:"info" choice:"debug" choice:"info" choice:"error" choice:"fatal" description:"Minimum level of logs to see."`
+	LogLevel   string `long:"log-level" default:"info" choice:"debug" choice:"info" choice:"error" choice:"fatal" description:"Minimum level of logs to see."`
+	TimeFormat string `long:"time-format" default:"unix-epoch" choice:"unix-epoch" choice:"rfc3339" description:"format of log timestamps."`
 }
 
 func (f LagerFlag) Logger(component string) (lager.Logger, *lager.ReconfigurableSink) {
@@ -33,9 +37,17 @@ func (f LagerFlag) Logger(component string) (lager.Logger, *lager.Reconfigurable
 		panic(fmt.Sprintf("unknown log level: %s", f.LogLevel))
 	}
 
+	var internalSink lager.Sink
+	switch f.TimeFormat {
+	case FormatRFC3339:
+		internalSink = lager.NewPrettySink(os.Stdout, lager.DEBUG)
+	default:
+		internalSink = lager.NewWriterSink(os.Stdout, lager.DEBUG)
+	}
+
 	logger := lager.NewLogger(component)
 
-	sink := lager.NewReconfigurableSink(lager.NewWriterSink(os.Stdout, lager.DEBUG), minLagerLogLevel)
+	sink := lager.NewReconfigurableSink(internalSink, minLagerLogLevel)
 	logger.RegisterSink(sink)
 
 	return logger, sink
