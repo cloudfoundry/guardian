1d7db05650fe87d5c83be35d36659733378fb2de
Tom Godkin <tgodkin@pivotal.io>
2018-07-02 11:54:12 +0100

    Remove unused test file.
    It seems ginkgo fails to compile test suites that only have _linux parts
    on Windows. We previously added an empty _test.go to get around this,
    but it seems this is leaving crap in C:\Windows\Temp\go-link-...\ on
    each run. This eventually causes the concourse worker to become too
    fragmented.
    
    Instead, we'll ignore the nerd suite in the script that runs these tests
    on Windows and I'll raise an issue(/PR?) to ginkgo to report the issue.
    
    [#156607857]

--- rundmc/runcontainerd/nerd/nerd_test.go
+++
@@ -1,8 +0,0 @@
-package nerd_test
-
-import (
-	. "github.com/onsi/ginkgo"
-)
-
-// Not sure why we need this, perhaps a ginkgo limitation that we need a non GOOS specific test file in this package
-var _ = Describe("Nerd Cross-Platform", func() {})
