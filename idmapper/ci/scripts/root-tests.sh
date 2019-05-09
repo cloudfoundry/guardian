df787f1c1794f6f8cbb114aee725401ca0a64241
Georgi Sabev <georgethebeatle@gmail.com>
2019-05-09 12:21:27 +0300

    Use garden-ci for idmapper test task
    Co-authored-by: Julia Nedialkova <julianedialkova@hotmail.com>

--- idmapper/ci/scripts/root-tests.sh
+++ idmapper/ci/scripts/root-tests.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
 set -e
 
-move_to_gopath() {
-  thing_i_want_moved=$1
-  dest_path=/go/src/code.cloudfoundry.org/${thing_i_want_moved}
-
-  # remove the original package path
-  [ -d $dest_path ] && rmdir $dest_path
-
-  # link the uploaded source (from build) to the GOPATH
-  ln -s $PWD/src/code.cloudfoundry.org/${thing_i_want_moved} $dest_path
-
-  # because the uploaded source is owned by the user that runs fly, we need
-  # to chown
-  pushd $dest_path
-    sudo chown -R groot:groot .
-  popd
-
-  echo $dest_path
-}
-dest_path=$(move_to_gopath idmapper)
-cd $dest_path
-
 echo "I AM ROOT" | grootsay
 
 ginkgo -mod vendor -p -race -r "$@"
