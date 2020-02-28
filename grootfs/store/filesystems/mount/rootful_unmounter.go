99cc3a714f95a202a93b00240a8873c6fa162b7c
Kieron Browne <kbrowne@pivotal.io>
2020-02-28 16:09:11 +0200

    Add retry logic around unmounting the image rootfs
    [#168988942]
    
    Co-authored-by: Yulia Nedyalkova <julianedialkova@hotmail.com>

--- grootfs/store/filesystems/mount/rootful_unmounter.go
+++ grootfs/store/filesystems/mount/rootful_unmounter.go
@@ -2,6 +2,7 @@ package mount
 
 import (
 	"os"
+	"time"
 
 	"golang.org/x/sys/unix"
 )
@@ -10,10 +11,21 @@ type RootfulUnmounter struct {
 }
 
 func (u RootfulUnmounter) Unmount(path string) error {
-	err := unix.Unmount(path, 0)
-	// do not error if unmountPath does not exist or is not a mountpoint
-	if os.IsNotExist(err) || err == unix.EINVAL {
-		return nil
+	var err error
+
+	for i := 0; i < 50; i++ {
+		err = unix.Unmount(path, 0)
+		// do not error if unmountPath does not exist or is not a mountpoint
+		if os.IsNotExist(err) || err == unix.EINVAL {
+			return nil
+		}
+
+		if err == nil {
+			return nil
+		}
+
+		time.Sleep(time.Millisecond * 100)
 	}
+
 	return err
 }
