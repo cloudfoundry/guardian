35ab6b04f1ddcc2656ea9cd1b0e2df42f9ae67f8
Danail Branekov <danailster@gmail.com>
2020-03-06 16:26:05 +0200

    Fix a flaky test
    the test may perform the check prior rebalancer is done, use
    `Eventually` to mitigate this
    
    [#171307969]

--- gqt/rebalancing_test.go
+++ gqt/rebalancing_test.go
@@ -123,7 +123,7 @@ var _ = Describe("CPU shares rebalancing", func() {
 				})
 
 				It("sets the bad cgroup shares proportionally", func() {
-					Expect(readCgroupFile(badCgroupPath, "cpu.shares")).To(BeNumerically("~", 2000, 1))
+					Eventually(readCgroupFile(badCgroupPath, "cpu.shares")).Should(BeNumerically("~", 2000, 1))
 				})
 			})
 		})
