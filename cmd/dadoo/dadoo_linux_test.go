8fedc1a369347cd453f24786ae05ede220b2119e
Yulia Nedyalkova <julianedialkova@hotmail.com>
2020-03-30 15:56:16 +0100

    Include debug logging if untar fails again
    [#172053976]

--- cmd/dadoo/dadoo_linux_test.go
+++ cmd/dadoo/dadoo_linux_test.go
@@ -57,7 +57,12 @@ var _ = Describe("Dadoo", func() {
 		Expect(os.MkdirAll(rootfsPath, 0700)).To(Succeed())
 		cp, err := gexec.Start(exec.Command("tar", "-xf", os.Getenv("GARDEN_TEST_ROOTFS"), "-C", rootfsPath), GinkgoWriter, GinkgoWriter)
 		Expect(err).NotTo(HaveOccurred())
-		Expect(cp.Wait().ExitCode()).To(Equal(0))
+
+		Expect(cp.Wait().ExitCode()).To(Equal(0), func() string {
+			lscmd := exec.Command("ls", "-l", filepath.Join(rootfsPath, "bin"))
+			out, _ := lscmd.CombinedOutput()
+			return fmt.Sprintf("Failed to untar rootfs.tar into %s. Contents of ./bin/:\n%s", rootfsPath, string(out))
+		})
 
 		chown, err := gexec.Start(exec.Command("chown", "-R", "1:1", filepath.Join(bundlePath, "root")), GinkgoWriter, GinkgoWriter)
 		Expect(err).NotTo(HaveOccurred())
