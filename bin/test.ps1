28385ff8ecee1561456b71bb4a7ed4956717cc7a
Marc Paquette <mpaquette@vmware.com>
2023-11-17 12:49:20 +0000

    Unset GARDEN_TEST_ROOTFS for windows
    Signed-off-by: Amin Jamali <ajamali@vmware.com>

--- bin/test.ps1
+++ bin/test.ps1
@@ -1,6 +1,7 @@
 $ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
+$env:GARDEN_TEST_ROOTFS = "N/A"
 Invoke-Expression "go run github.com/onsi/ginkgo/v2/ginkgo $args --skip-package dadoo,gqt,kawasaki,locksmith,socket2me,signals,runcontainerd\nerd"
 if ($LastExitCode -ne 0) {
   throw "tests failed"
