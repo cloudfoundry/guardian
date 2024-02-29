5860f123df679e4c3fd1031a0d2b71c4f7ee0e14
Geoff Franks <geoff.franks@broadcom.com>
2024-02-29 15:48:15 +0000

    Remove unused code

--- rundmc/preparerootfs/prepare_windows.go
+++ rundmc/preparerootfs/prepare_windows.go
@@ -5,28 +5,6 @@ import (
 	"os/exec"
 )
 
-const name = "prepare-rootfs"
-
-func init() {
-}
-
 func Command(rootfsPath string, uid, gid int, mode os.FileMode, recreate bool, paths ...string) *exec.Cmd {
 	return &exec.Cmd{}
-}
-
-func prepare() {
-}
-
-func rmdir(path string) {
-}
-
-func mkdir(path string, uid, gid int, mode os.FileMode) {
-}
-
-func isSymlink(stat os.FileInfo) bool {
-	return false
-}
-
-func mustReadSymlink(path string) string {
-	return ""
 }
