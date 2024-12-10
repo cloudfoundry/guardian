d6fa43799d38c717b5f5a8bd9df04fa381708c7d
Marc Paquette <marc.paquette@broadcom.com>
2024-12-10 19:20:17 +0000

    Remove Containerd binary imports
    In containerd 2.0 we no longer need to build containerd. We are now
    going to submodule it within the garden-runc-release for the binaries.

--- tools/tools.go
+++ tools/tools.go
@@ -4,9 +4,6 @@
 package main
 
 import (
-	_ "github.com/containerd/containerd/v2/cmd/containerd"
-	_ "github.com/containerd/containerd/v2/cmd/containerd-shim-runc-v2"
-	_ "github.com/containerd/containerd/v2/cmd/ctr"
 	_ "github.com/opencontainers/runc"
 
 	_ "github.com/maxbrunsfeld/counterfeiter/v6"
