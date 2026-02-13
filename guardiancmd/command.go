646415b84313a93f98401b0fdee3368c38739bb4
Maria Shaldybin <maria.shaldybin@broadcom.com>
2026-02-13 01:43:50 +0000

    Revert "enable cgroupns on unified systems; mount cgroupv2 inside container"
    This reverts commit d1e0751e791484d41026419b56b2249d79a20368.

--- guardiancmd/command.go
+++ guardiancmd/command.go
@@ -46,7 +46,6 @@ import (
 	"code.cloudfoundry.org/localip"
 	"github.com/eapache/go-resiliency/retrier"
 	uuid "github.com/nu7hatch/gouuid"
-	"github.com/opencontainers/cgroups"
 
 	"github.com/opencontainers/runtime-spec/specs-go"
 )
@@ -514,10 +513,6 @@ func (cmd *CommonCommand) wireContainerizer(
 		WithRootFS(cmd.Containers.DefaultRootFS).
 		WithProcess(baseProcess).
 		WithRootFSPropagation("private")
-
-	if cgroups.IsCgroup2UnifiedMode() {
-		baseBundle = baseBundle.WithNamespace(goci.CgroupNamespace)
-	}
 
 	uidMappings, gidMappings := cmd.idMappings()
 	unprivilegedBundle := baseBundle.
--- guardiancmd/command_linux.go
+++ guardiancmd/command_linux.go
@@ -215,10 +215,14 @@ func privilegedMounts() []specs.Mount {
 }
 
 func unprivilegedMounts() []specs.Mount {
-	return []specs.Mount{
+	mounts := []specs.Mount{
 		{Destination: "/proc", Type: "proc", Source: "proc", Options: []string{"nosuid", "noexec", "nodev"}},
-		{Destination: "/sys/fs/cgroup", Type: "cgroup", Source: "cgroup", Options: []string{"ro", "nosuid", "noexec", "nodev"}},
 	}
+
+	if !cgroups.IsCgroup2UnifiedMode() {
+		mounts = append(mounts, specs.Mount{Destination: "/sys/fs/cgroup", Type: "cgroup", Source: "cgroup", Options: []string{"ro", "nosuid", "noexec", "nodev"}})
+	}
+	return mounts
 }
 
 func getPrivilegedDevices() []specs.LinuxDevice {
