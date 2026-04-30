e728a662ae01c53ebf0c4961a8629680d5971d1c
Jochen Ehret <jochen.ehret@sap.com>
2026-04-30 12:01:12 +0200

    Add NEWCGROUP to privileged container namespaces
    * Without a cgroup namespace, Garden containers share the host's cgroup
      tree. On cgroup v2, this causes BPM's runc to fail with "container's
      cgroup is not empty" when a deployed job has the same name as a job
      running on a BOSH Lite director (e.g. the "uaa" job).

--- guardiancmd/server.go
+++ guardiancmd/server.go
@@ -91,7 +91,7 @@ var privilegedMaxCaps = []string{
 var nonRootMaxCaps = append(unprivilegedMaxCaps, "CAP_SYS_ADMIN")
 
 var PrivilegedContainerNamespaces = []specs.LinuxNamespace{
-	goci.NetworkNamespace, goci.PIDNamespace, goci.UTSNamespace, goci.IPCNamespace, goci.MountNamespace,
+	goci.NetworkNamespace, goci.PIDNamespace, goci.UTSNamespace, goci.IPCNamespace, goci.MountNamespace, goci.CgroupNamespace,
 }
 
 var (
